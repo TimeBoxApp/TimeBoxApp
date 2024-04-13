f1ffd02d2a4e49d17bf8f5e0d25db9e56f122028
Roman Kulyk <roman.kulyk@romankulyk.local>
2024-04-13 23:39:51 +0300

    Add build optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,159 @@
-const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const JavaScriptObfuscator = require('webpack-obfuscator');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const AutoPrefixer = require('autoprefixer');
+const CssNano = require('cssnano');
+const LoadablePlugin = require('@loadable/webpack-plugin');
 
-module.exports = {
-  mode: 'production',
+const inlineSvgConfig = {
+  test: /\.svg$/,
+  use: [
+    {
+      loader: '@svgr/webpack',
+      options: {
+        exportType: 'named'
+      }
+    }
+  ],
+  exclude: /node_modules/
+};
+
+const sassProdRules = [
+  {
+    test: /\.scss$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      'css-loader', // importLoaders should be used with options object if needed
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: [AutoPrefixer(), CssNano({ preset: 'default' })]
+          }
+        }
+      },
+      'sass-loader'
+    ],
+    exclude: /\.module\.scss$/
+  },
+  {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader']
+  },
+  {
+    test: /\.module\.scss$/,
+    use: [
+      MiniCssExtractPlugin.loader,
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            mode: 'local',
+            localIdentName: '[hash:base64]'
+          }
+        }
+      },
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            plugins: [AutoPrefixer(), CssNano({ preset: 'default' })]
+          }
+        }
+      },
+      'sass-loader'
+    ]
+  }
+];
+
+const clientCommon = {
   entry: './src/index.js',
+
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js'
   },
+
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: 'all',
+      name: false
+    }
+  },
+
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        test: /\.(jsx|js)$/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
+        },
+        exclude: /node_modules/
+      },
+      inlineSvgConfig,
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|otf|ico)$/,
+        exclude: /\.svg?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/'
+          }
         }
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // 3. Inject styles into DOM
-          'css-loader', // 2. Turns css into commonjs
-          'sass-loader' // 1. Turns sass into css
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              exportType: 'named' // This option might be needed depending on version or specific preferences
-            }
-          }
-        ]
-      }
+      ...sassProdRules
     ]
   },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin({}), new CssMinimizerPlugin()]
-  },
+
   resolve: {
     extensions: ['.js', '.jsx']
-  }
+  },
+
+  plugins: [
+    new LoadablePlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.join(__dirname, 'public/index.html'),
+      inject: true,
+      minify: {
+        collapseWhitespace: true
+      }
+    })
+  ]
 };
+
+const clientProdConfig = merge(clientCommon, {
+  mode: 'production',
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+
+    new JavaScriptObfuscator({
+      rotateUnicodeArray: true,
+      debugProtection: true,
+      debugProtectionInterval: 0,
+      selfDefending: false,
+      disableConsoleOutput: true,
+      domainLock: ['.timebox.guru', 'timebox.guru']
+    }),
+
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled'
+    })
+  ]
+});
+
+module.exports = clientProdConfig;
